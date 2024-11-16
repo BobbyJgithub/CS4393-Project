6559016573c4eb1a8ff76baaac21cb06f91a0539
BobbyJgithub <126908477+BobbyJgithub@users.noreply.github.com>
2024-11-15 19:36:52 -0600

    Ticket Details Shown on Profile

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -3,17 +3,29 @@ import { useSelector, useDispatch } from 'react-redux';
 import { removeFromCart, clearCart } from '../../redux/slices/cartSlice';
 import CartItem from '../../components/CartItem/CartItem';
 import styles from './Cart.module.css';
+import { addPurchasedTicket } from '../../utils/auth';
 
 const Cart = () => {
   const dispatch = useDispatch();
   const { items, total } = useSelector(state => state.cart);
+  const { user } = useSelector(state => state.auth);
 
   const handleRemoveItem = (itemId) => {
     dispatch(removeFromCart(itemId));
   };
 
   const handleCheckout = () => {
-    alert('This is a mock checkout. In a real app, this would proceed to payment.');
+    if (!user) {
+      alert('Please login to checkout');
+      return;
+    }
+
+    // Save each ticket to user's purchase history
+    items.forEach(ticket => {
+      addPurchasedTicket(user.id, ticket);
+    });
+
+    alert('Purchase successful! Check your profile for ticket details.');
     dispatch(clearCart());
   };
 
